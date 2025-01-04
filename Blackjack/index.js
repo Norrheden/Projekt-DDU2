18d14bd855bc5ee1b237141bbabfbd1e1553633c
Norrheden <eliasnorrhedenmau@gmail.com>
2025-01-04 21:15:10 +0100

    Blackjack: Adjustments to HTML
    Deleted the class cardsWhiteBorder and put my text to absolute in css. Also added dealCardToBank function.

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -4,16 +4,24 @@
 const placeBetInputDOM = document.getElementById("placeBetInput")
 const placeBetButtonDOM = document.getElementById("placeBetButton");
 const walletMoneyDOM = document.getElementById("walletMoney");
-const playerHand = document.getElementById("playerHand");
+const playerHandDOM = document.getElementById("playerHand");
+const bankHandDOM = document.getElementById("bankHand")
 
 placeBetButtonDOM.addEventListener("click",function(){
     if(placeBetInputDOM.value >= 10 && Number(walletMoneyDOM.textContent) >= Number(placeBetInputDOM.value)) {
+        deck = []
         walletMoneyDOM.textContent = `${Number(walletMoneyDOM.textContent) - Number(placeBetInputDOM.value)}`
-        let deck = getDeck()
+        deck = getDeck()
         dealCardToPlayer(2, deck)
+        dealCardToBank(1, deck)
 
     }
 })
+placeBetButtonDOM.addEventListener("keydown", function(event) {
+    if (event.key === "") {
+        event.preventDefault();
+    }
+});
 
 
 
@@ -22,13 +30,12 @@ placeBetButtonDOM.addEventListener("click",function(){
 
 
 
+let deck = [];
 
 
 
-
-const colors = ["Clubs", "Diamonds", "Spades", "Hearts"]
+const colors = ["Club", "Diamond", "Spade", "Heart"]
 function getDeck(){
-    const deck = [];
     for(let color of colors) {
         for(let i = 1; i<=13; i++) {
             deck.push({
@@ -40,15 +47,29 @@ function getDeck(){
     return deck;
 
 }
-function dealCardToPlayer(nCards,inDeck) {
+function dealCardToPlayer(nCards,deck) {
     for(let i = 0; i<nCards; i++) {
-        const index = randNumber(inDeck)
-        const card = inDeck[index];
+        const index = randNumber(deck)
+        const card = deck[index];
+        deck.splice(index,1)
         let cardDiv = document.createElement("div");
         cardDiv.classList.add("cards")
-        cardDiv.textContent = `${card.color} ${card.value}`
-        playerHand.appendChild(cardDiv);
+        cardDiv.innerHTML = `${card.color}<br>${card.value}`
+        playerHandDOM.appendChild(cardDiv);
     }
+    console.log(deck)
+}
+function dealCardToBank(nCards,deck) {
+    for(let i = 0; i<nCards; i++) {
+        const index = randNumber(deck)
+        const card = deck[index];
+        deck.splice(index,1)
+        let cardDiv = document.createElement("div");
+        cardDiv.classList.add("cards")
+        cardDiv.innerHTML = `${card.color}<br>${card.value}`
+        bankHandDOM.appendChild(cardDiv);
+    }
+    console.log(deck)
 }
 function randNumber(array) {
     return  Math.floor((array.length) * Math.random())
