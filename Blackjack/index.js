e804e9e29ffc5de75edd13d8a1de98f882fe7177
Norrheden <eliasnorrhedenmau@gmail.com>
2025-01-04 20:03:29 +0100

    Blackjack: dealCardToPlayer

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -3,11 +3,15 @@
 
 const placeBetInputDOM = document.getElementById("placeBetInput")
 const placeBetButtonDOM = document.getElementById("placeBetButton");
-
+const walletMoneyDOM = document.getElementById("walletMoney");
+const playerHand = document.getElementById("playerHand");
 
 placeBetButtonDOM.addEventListener("click",function(){
-    if(placeBetInputDOM.value >= 10) {
-        console.log(placeBetInputDOM.value)
+    if(placeBetInputDOM.value >= 10 && Number(walletMoneyDOM.textContent) >= Number(placeBetInputDOM.value)) {
+        walletMoneyDOM.textContent = `${Number(walletMoneyDOM.textContent) - Number(placeBetInputDOM.value)}`
+        let deck = getDeck()
+        dealCardToPlayer(2, deck)
+
     }
 })
 
@@ -36,4 +40,16 @@ function getDeck(){
     return deck;
 
 }
-
+function dealCardToPlayer(nCards,inDeck) {
+    for(let i = 0; i<nCards; i++) {
+        const index = randNumber(inDeck)
+        const card = inDeck[index];
+        let cardDiv = document.createElement("div");
+        cardDiv.classList.add("cards")
+        cardDiv.textContent = `${card.color} ${card.value}`
+        playerHand.appendChild(cardDiv);
+    }
+}
+function randNumber(array) {
+    return  Math.floor((array.length) * Math.random())
+}
